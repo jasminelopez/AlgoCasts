1addca9309f6eefc053377a8a966d7eeb611bed5
jasminelopez <lopez.jasmine094@gmail.com>
2019-11-12 11:13:37 -0800

    pass tests for pyramid utilizing iterative solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,26 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+
+  //loop through the number of n times
+    // for each iteration, we loop again
+    // this time we add 2 # symbols on either side
+    //but the previous # gets spaces added to it as well.
+
+      // n = 3
+      const midpoint = Math.floor((2 * n - 1) / 2);
+    for (let row = 0; row < n; row++) {
+      let level = '';
+      for (let column = 0; column < 2 * n -1; column++) {
+        if (midpoint - row <= column  && midpoint + row >= column) {
+          level += '#';
+        } else {
+          level += ' ';
+        }
+      }
+      console.log(level);
+    }
+}
 
 module.exports = pyramid;
